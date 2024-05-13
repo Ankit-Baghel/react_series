0f70201ff94108471b3ba0388c34f5d208ee4891
Ankit-Baghel <117919740+Ankit-Baghel@users.noreply.github.com>
2024-05-13 17:49:24 +0530

    added comment

--- 03TalwindProps/src/App.jsx
+++ 03TalwindProps/src/App.jsx
@@ -13,7 +13,7 @@ let myObj={
     <>
       <h1 className='bg-4 bg-green-300 text-black p-4 rounded-xl mb-4'>Tailwind css</h1>
       <Card channel ="ankit baghel" btnText="click me"/>
-      <Card channel="aman" />// btntext is not passed so it will take default value
+      <Card channel="aman" />{/*btntext is not passed so it will take default value*/}
       
     </>
   )
